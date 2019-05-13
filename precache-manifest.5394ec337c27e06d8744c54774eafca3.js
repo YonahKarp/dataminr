1c840f6b984404bea53aec0fe8b1dc4babe341d5
Yonah Karp <ykarp@student.touro.edu>
2019-05-13 09:58:07 -0400

    Updates

---
+++ precache-manifest.5394ec337c27e06d8744c54774eafca3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4b056271f6b83f94b320a105f178725",
+    "url": "/dataminr/index.html"
+  },
+  {
+    "revision": "3c764b6ce94575d787e3",
+    "url": "/dataminr/static/css/main.a4651c12.chunk.css"
+  },
+  {
+    "revision": "971dee3dbc9ced842233",
+    "url": "/dataminr/static/js/2.d0a03733.chunk.js"
+  },
+  {
+    "revision": "3c764b6ce94575d787e3",
+    "url": "/dataminr/static/js/main.78606e0f.chunk.js"
+  },
+  {
+    "revision": "57340a30bc7ee7cb5504",
+    "url": "/dataminr/static/js/runtime~main.a98cf92b.js"
+  }
+]);
